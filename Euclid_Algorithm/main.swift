6707f70924a4bbfe2ada134f38a07a03951428a1
Rich Blanchard <blanchard5595@yahoo.com>
2015-09-18 12:40:49 -0600

    Initial Commit

---
+++ Euclid_Algorithm/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Euclid_Algorithm
+//
+//  Created by Rich Blanchard on 9/18/15.
+//  Copyright © 2015 Rich. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
