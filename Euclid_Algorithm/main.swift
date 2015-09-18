7a6937f3f08b952900950cb658c5b5125473ebf7
Rich Blanchard <blanchard5595@yahoo.com>
2015-09-18 14:12:05 -0600

    Get Binary Representation of a number

--- Euclid_Algorithm/main.swift
+++ Euclid_Algorithm/main.swift
@@ -7,6 +7,45 @@
 //
 
 import Foundation
+func newString(stringToReverse:String)->String {
+    var reversedString = String()
+    for var index = stringToReverse.characters.count - 1; index >= 0; index-- {
+        let backwardsChar = [Character](stringToReverse.characters)
+        reversedString.appendContentsOf("\(backwardsChar[index])")
+      
+    }
+    return reversedString
+}
 
-print("Hello, World!")
-
+func getBinary(base: Int,number: Int )->Void {
+   
+   var quotient = base
+    var nominator = number
+    var newNumberString = ""
+    while nominator > 0 {
+       
+        var remainder = Int(nominator % quotient)
+        var correctNominator = (nominator - remainder) / quotient
+        newNumberString.appendContentsOf("\(remainder)")
+        
+        nominator = correctNominator
+    }
+    let String = newString(newNumberString)
+    print(String)
+    
+}
+func remainderHelper(remainder: Double, base: Double) {
+    let power = base
+    var newRemainder = String()
+    var numberToBeWritten = remainder
+    for  var i = power; i >= 0; i-- {
+        let divisor = pow(power, i)
+        numberToBeWritten /= divisor
+        let newNumber = floor(numberToBeWritten)
+        newRemainder.appendContentsOf("\(newNumber)")
+        
+    }
+    print("\(newRemainder)")
+    
+}
+getBinary(2, number: 10)
